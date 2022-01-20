20899535abe55a84016e810e94932324c7433459
liyang <ylikp@connect.ust.hk>
2022-01-20 17:58:29 +0800

    init forge

--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -3,10 +3,21 @@ pragma solidity 0.8.10;
 
 import "ds-test/test.sol";
 
+import "../Contract.sol";
+
 contract ContractTest is DSTest {
-    function setUp() public {}
+    Contract myContract;
+
+    function setUp() public {
+        myContract = new Contract();
+    }
 
     function testExample() public {
         assertTrue(true);
+    }
+
+    function testAddOne(uint256 x) public {
+        emit log_string("hello");
+        assertEq(x + 1, myContract.addOne(x));
     }
 }
